d8be4ca0d0b4a910ab5e9d1132bffdda674ef435
ShanuSaini <shanusaini434@gmail.com>
2019-05-31 20:52:55 -0400

    Added Recipe Model, Creating Dynamic listing using this model,
    Added Recipe Model
    Creating Dynamic listing using this model
    Property binding and strind interpolation

--- lp-one/src/app/recipes/recipes-list/recipes-list.component.ts
+++ lp-one/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
+
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html'
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Boondi Ke Ladoo', 
+      'Boondi Ke Ladoo Recipe', 
+      'https://justhindi.in/recipes/wp-content/uploads/2017/04/boondi-ke-ladoo.jpg'
+    )
+  ];
 
   constructor() { }
 
