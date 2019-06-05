7aaec533c4ffd6de48e00909d778cf1186fb9e7c
ShanuSaini <shanusaini434@gmail.com>
2019-06-05 08:21:27 -0400

    Manage -> Ingredients To shoping list
    Manage -> Ingredients To shoping list

--- lp-one/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ lp-one/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe-model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShopingListService } from 'src/app/shoping-list/shoping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +11,15 @@ export class RecipeDetailComponent implements OnInit {
 
   @Input() recipeDetail: Recipe;
 
-  constructor() { }
+  constructor(private slService: ShopingListService) { }
 
   ngOnInit() {
+  }
+
+  toShopingList(ingredients: Ingredient[]){
+    for(let ingredient of ingredients){
+      this.slService.addIngredient(ingredient);
+    }
   }
 
 }
--- lp-one/src/app/recipes/recipes-service.service.ts
+++ lp-one/src/app/recipes/recipes-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe-model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 @Injectable({
@@ -13,12 +14,26 @@ export class RecipesServiceService {
     new Recipe(
       'Boondi Ke Ladoo', 
       'Boondi Ke Ladoo Recipe', 
-      'assets/images/recipes/boondi-ke-ladoo.jpg'
+      'assets/images/recipes/boondi-ke-ladoo.jpg',
+      [
+        new Ingredient( 'Flour 250g', 10 ),
+        new Ingredient( 'Semolina 50g', 15 ),
+        new Ingredient( 'Sugar 400g', 20 ),
+        new Ingredient( 'Water 2 Liters', 0 ),
+        new Ingredient( 'Yellow Food Color', 10 )
+      ]
     ),
     new Recipe(
       'Boondi Ke Ladoo 2', 
       'Boondi Ke Ladoo Recipe 2', 
-      'assets/images/recipes/boondi-ke-ladoo.jpg'
+      'assets/images/recipes/boondi-ke-ladoo.jpg',
+      [
+        new Ingredient( 'Flour 250g', 10 ),
+        new Ingredient( 'Semolina 50g', 15 ),
+        new Ingredient( 'Sugar 400g', 20 ),
+        new Ingredient( 'Water 2 Liters', 0 ),
+        new Ingredient( 'Yellow Food Color', 10 )
+      ]
     )
   ];
 
