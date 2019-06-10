819aa8411b5544378c4ac5c10d9109178fafe08a
ShanuSaini <shanusaini434@gmail.com>
2019-06-10 07:39:15 -0400

    Adding Routing for root components
    Adding Routing for root components

--- lp-one/src/app/app-routing.module.ts
+++ lp-one/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shoping-list', component: ShopingListComponent },
+  
+  { path: '**', redirectTo: 'recipes' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
