53469dcdbf8532aedfeb94e81ea746510901fdb3
ShanuSaini <shanusaini434@gmail.com>
2019-07-14 09:42:01 -0400

    basic Post, Get and Transforming data using rxjs map function
    basic Post, Get and Transforming data using rxjs map function

--- lp-http/src/app/app.component.ts
+++ lp-http/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,15 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() { 
+    this.onFetchPosts(); 
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'yourDBURL/posts.json',  // Realtime database URL here
         postData
       )
       .subscribe(responseData => {
@@ -26,7 +29,21 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.http.get('yourDBURL/posts.json')  // Realtime DataBase URL
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        }
+      );
   }
 
   onClearPosts() {
